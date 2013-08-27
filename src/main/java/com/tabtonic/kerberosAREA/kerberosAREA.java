4337a46788e2728cce03b3fae2373973f87da525
Steve Kallestad <steve@tabtonic.com>
2013-08-27 11:52:15 -0700

    copyright notice, inline documentation

--- src/main/java/com/tabtonic/kerberosAREA/kerberosAREA.java
+++ src/main/java/com/tabtonic/kerberosAREA/kerberosAREA.java
@@ -1,3 +1,23 @@
+/**
+ *    Copyright (C) 2013 TabTonic LLC
+ * 
+ *    This file is part of kerberosAREA.
+ *
+ *    kerberosAREA is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    kerberosAREA is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with kerberosAREA.  If not, see <http://opensource.org/licenses/gpl-3.0.html>.
+ * 
+ */
+	
 package com.tabtonic.kerberosAREA;
 
 
@@ -20,13 +40,14 @@ import com.bmc.arsys.pluginsvr.plugins.ARPluginContext;
 import com.bmc.arsys.pluginsvr.plugins.ARPluginInfo;
 
 /**
- * <h4>Description</h4>
- *
+ * <h4>kerberosAREA</h4>
+ * This is an AREA plugin designed for use within the BMC Remedy Action Request System.
+ * <br>AREA plugins are a used for delegating authentication responsibility to third party tools.  This plugin receives 
+ * authentication credentials and passes them to MIT Kerberos for validation.
  * 
  * @author      Steve Kallestad, <a href="http://www.tabtonic.com/">TabTonic LLC</a>
  *
- * @version     0.0.1-SNAPSHOT
- *       
+ * @version     0.0.1-SNAPSHOT      
  */
 public class kerberosAREA extends AREAPlugin {
 	/**
@@ -41,6 +62,15 @@ public class kerberosAREA extends AREAPlugin {
 	public kerberosAREA(){
 		
 	}
+	
+	/**
+	 * The initialize function is called one time on system startup.
+	 * 
+	 * It is being utilized to load configuration parameters.
+	 * 
+	 * @param context
+	 * @throws ARException
+	 */
 	@Override
 	public void initialize(ARPluginContext context) throws ARException {
 		context.logMessage(pluginInfo, ARPluginContext.PLUGIN_LOG_LEVEL_INFO, "Initializing kerberosAREA");
@@ -77,21 +107,41 @@ public class kerberosAREA extends AREAPlugin {
 		
 		super.initialize(context);
 	}
+	
+	/**
+	 * The AR Server will check with the AREA plugin to determine if user information has expired.
+		 it does this when it receives a second authentication request at least five minutes after the
+		 previous authentication request.
+		
+		 We always return true in this instance, invalidating any existing user cache info.
+	 * @param context
+	 * @return
+	 * @throws ARException
+	 */
 	@Override
 	public boolean areaNeedSync(ARPluginContext context) throws ARException {
-		// The AR Server will check with the AREA plugin to determine if user information has expired.
-		// it does this when it receives a second authentication request at least five minutes after the
-		// previous authentication request.
-		//
-		// We always return true in this instance, invalidating any existing user cache info.
+		 
 		return true;
 	}
 
+	/**
+	 * The areaVerifyLogin function is called by Remedy with the following parameters.  
+	 * 
+	 * We return an AREAResponse object which will tell remedy if the authentication is a success or a failure.
+	 * 
+	 * @param context
+	 * @param user
+	 * @param password
+	 * @param networkAddress
+	 * @param authString
+	 * @return
+	 * @throws ARException
+	 */
 	@Override
 	public AREAResponse areaVerifyLogin(ARPluginContext context, String user,
 			String password, String networkAddress, String authString) throws ARException {
 		logger.trace("Verify login for {} from {}", user, networkAddress);
-		
+
 		//we need to return an AREAResponse object
 		AREAResponse response = new AREAResponse();
 		
